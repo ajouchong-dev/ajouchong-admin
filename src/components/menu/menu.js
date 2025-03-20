527ac8161bb689b52e7801caf6eed68699ddac8c
y.tnwjd <ysjsw@ajou.ac.kr>
2025-03-20 20:58:06 +0900

    feat: make side menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,25 +3,16 @@ import './menu.css';
 
 const Menu = () => {
     return (
-      <header className="main-header">
-        <div className="top-bar">
-          <div className="top-links">
-            <span><a href='/'>메인 페이지</a></span>
-          </div>
-        </div>
-        <div className="logo">
-          <img src="aurum_white.png" alt="아우름 로고" className="logo-image" /> {/* 로고 이미지 추가 */}
-        </div>
-        <nav className="main-nav">
-          <ul>
-            <li><a href="/introduce">소개</a></li>
-            <li><a href="/notice">소식</a></li>
-            <li><a href="/communicate">소통</a></li>
-            <li><a href="/reference">자료실</a></li>
-            <li><a href="/welfare">학생복지</a></li>
+      <div id="main-menu">
+          <ul className="side-menu">
+              <li><a href="/member">회원 관리</a></li>
+              <li><a href="/notice">공지사항</a></li>
+              <li><a href="/qna">Q&A</a></li>
+              <li><a href="/agora">100인 안건 상정제</a></li>
+              <li><a href="/data">세칙 및 회칙</a></li>
+              <li><a href="/partnership">제휴백과</a></li>
           </ul>
-        </nav>
-      </header>
+      </div>
     );
 }
 
