8486bc96bf78c4d34b11372bbf7936952c0ca324
y.tnwjd <ysjsw@ajou.ac.kr>
2025-03-21 15:56:38 +0900

    feat: login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,25 +1,118 @@
-import React from 'react';
-import './login.css'; // 스타일링 파일이 필요하다면 임포트
+import React, {useEffect} from 'react';
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import Cookies from "js-cookie";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
+import './login.css';
 
-const Login = () => {
+const Login = ({ user, setUser }) => {
+
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem("user", JSON.stringify(user));
+        }
+    }, [user]);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, [setUser]);
+
+    const signInWithGoogle = useGoogleLogin({
+        onSuccess: async (tokenResponse) => {
+            try {
+                const { data: userInfo } = await axios.get(
+                    "https://www.googleapis.com/oauth2/v3/userinfo",
+                    {
+                        headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+                    }
+                );
+
+                // console.log("Google User Info:", userInfo);
+
+                const refreshToken = Cookies.get("refreshToken") || null;
+
+                const { data: backendData } = await axios.post(
+                    `${BASE_URL}/api/login/auth/oauth`,
+                    {
+                        accessToken: tokenResponse.access_token,
+                        refreshToken: refreshToken,
+                    },
+                    {
+                        withCredentials: true,
+                        headers: { "Content-Type": "application/json" },
+                    }
+                );
+
+                const { status, data } = backendData;
+                const jwtToken = data.jwtToken;
+
+                if (jwtToken) {
+
+                    setUser(userInfo);
+                    localStorage.setItem("user", JSON.stringify(userInfo));
+
+                    navigate("/member");
+                } else {
+                    console.error("JWT가 존재하지 않습니다.");
+                }
+            } catch (error) {
+                if (error.response?.status === 401) {
+                    console.error("JWT 만료됨 - 로그아웃 처리");
+                    logout();
+                } else {
+                    console.error("Login Error:", error);
+                }
+            }
+        },
+        onError: (error) => console.error("Login Failed:", error),
+        scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+        access_type: "offline",
+        prompt: "consent",
+        hosted_domain: "ajou.ac.kr",
+    });
+
+    const logout = async () => {
+        try {
+            await axios.post(`${BASE_URL}/api/login/auth/logout`,
+                {},
+                { withCredentials: true }
+            );
+
+            googleLogout();
+            setUser(null);
+
+            localStorage.removeItem("user");
+            Cookies.remove("refreshToken");
+            Cookies.remove("accessToken");
+
+            navigate("/");
+        } catch (error) {
+            console.error("Logout error:", error);
+        }
+    };
+
     return (
-        <>
-              <main className="login-section">
-        <form className="login-form">
-          <h2>AJOU UNIV</h2>
-          <br></br>
-          <h1>관리자 계정 로그인</h1>
-          <br></br>
-          <div className="login-input">
-            <input type="email" placeholder="email" className="input-field" />
-            <input type="password" placeholder="password" className="input-field" />
-            <button type="submit" className="login-button">log in</button>
-          </div>
-        </form>
-      </main>
-        </>
+        <div className="login-container">
+            <h1>Ajouchong Admin</h1>
+            {user ? (
+                <button className="auth-button" onClick={logout}>
+                    Logout
+                </button>
+            ) : (
+                <button onClick={signInWithGoogle} className="auth-button">
+                    Login
+                </button>
+            )}
+        </div>
     );
-}
+};
 
 export default Login;
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './menu.css';
+import Login from "../login/Login";
+import {useAuth} from "../../context/AuthContext";
 
 const Menu = () => {
+    const { auth } = useAuth();
+    const [user, setUser] = useState(auth.user);
+
     return (
       <div id="main-menu">
+          <Login user={user} setUser={setUser} />
           <ul className="side-menu">
               <li><a href="/member">회원 관리</a></li>
               <li><a href="/notice">공지사항</a></li>
